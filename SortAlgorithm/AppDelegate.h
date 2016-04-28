61321ab3a506d50959cd22c17c2b9334b2688958
Leakey Fo <foleakey@gmail.com>
2016-04-29 00:14:01 +0800

    Quick finished, to be debug.

--- SortAlgorithm/AppDelegate.h
+++ SortAlgorithm/AppDelegate.h
@@ -11,10 +11,28 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>{
     IBOutlet NSButton *gerbtn,*readbtn;
     IBOutlet NSTextField *contxt,*maxtxt,*pathtxt;
+//    NSMutableArray *empty;
 }
 -(IBAction)gerrandarray:(id)sender;
 -(IBAction)redfromfile:(id)sender;
 -(void)quicksort:(NSMutableArray*)array;
 -(void)bubblesort:(NSMutableArray*)array;
--(NSMutableArray*)cpyarray:(NSMutableArray*)array;
+-(void)sleepsort:(NSMutableArray*)array;
+-(NSMutableArray*)cpyarray:(NSMutableArray*)array from:(int)min to:(int)max;
+-(void)BucketSort:(NSMutableArray *)array;
+-(void)SleepSort:(NSMutableArray *)array;
+-(void)RadixSprt:(NSMutableArray *)array;//基数排序
+-(void)PigeonholeSort:(NSMutableArray *)array;//鸽巢排序
+-(void)MergeSort:(NSMutableArray *)array;//归并排序
+-(void)SelectSort:(NSMutableArray *)array;//选择排序
+-(void)CocktailSort:(NSMutableArray *)array;//鸡尾酒排序
+-(void)ShellSort:(NSMutableArray *)array;//希尔排序
+-(void)HeapSort:(NSMutableArray *)array;//堆排序
+-(void)GnomeSort:(NSMutableArray *)array;//地精排序
+-(void)OddevenSort:(NSMutableArray *)array;//奇偶排序
+-(void)CombSort:(NSMutableArray *)array;//梳排序
+-(void)PatientSort:(NSMutableArray *)array;//耐心排序
+-(void)BeadSort:(NSMutableArray *)array;//珠排序
+-(void)CountSort:(NSMutableArray *)array;//计数排序
+
 @end
--- SortAlgorithm/AppDelegate.m
+++ SortAlgorithm/AppDelegate.m
@@ -16,6 +16,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+//    empty=[[NSMutableArray alloc]init];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
@@ -30,17 +31,26 @@
 //        NSLog(@"%@",[array objectAtIndex:i]);
     }
 //    [self quicksort:[self cpyarray:array]];
-    [self bubblesort:[self cpyarray:array]];
+//    [self bubblesort:[self cpyarray:array]];
+    array=[self cpyarray:array from:0 to:[array count]-1];
+    [self sleepsort:array];
+    sleep(15);
+    for (int i=0; i<20; i++) {
+        NSLog(@"%@",[array objectAtIndex:i]);
+    }
 }
 -(IBAction)redfromfile:(id)sender{
     
 }
--(NSMutableArray*)cpyarray:(NSMutableArray*)array{
-    NSMutableArray *tmp=[NSMutableArray arrayWithArray:array];
+-(NSMutableArray *)cpyarray:(NSMutableArray *)array from:(int)min to:(int)max{
+    NSMutableArray *tmp=[[NSMutableArray alloc]init];
+    for(int i=min;i<=max;i++){
+        [tmp addObject:[array objectAtIndex:i]];
+    }
     return tmp;
 }
 -(void)quicksort:(NSMutableArray*)array{ //to be done
-    NSMutableArray *tmp=[self cpyarray:array];
+    NSMutableArray *tmp=[self cpyarray:array from:0 to:[array count]-1];
     int right=[tmp count]-1,left=1;
     int pvio=[[tmp objectAtIndex:0]integerValue];
     while(left!=right){
@@ -55,10 +65,25 @@
             right--;
         }
     }
-    [tmp exchangeObjectAtIndex:0 withObjectAtIndex:left];
+    if ([[tmp objectAtIndex:left]intValue]<pvio) {
+        [tmp exchangeObjectAtIndex:0 withObjectAtIndex:left];
+    }
+    for (int i=0; i<[tmp count]; i++) {
+        NSLog(@"%d : %@",i,[tmp objectAtIndex:i]);
+    }
+    if(left>=2){
+        [self quicksort:[self cpyarray:tmp from:0 to:left]];
+    }
+    if(right-left>=3){
+        [self quicksort:[self cpyarray:tmp from:left to:right-1]];
+    }
+    for (int i=0; i<[tmp count]; i++) {
+        NSLog(@"out %d : %@",i,[tmp objectAtIndex:i]);
+    }
+    return;
 }
 -(void)bubblesort:(NSMutableArray*)array{ //done
-    NSMutableArray *tmp=[self cpyarray:array];
+    NSMutableArray *tmp=[self cpyarray:array from:0 to:[array count]-1];
     int counter=[tmp count];
     for (int i=0; i<counter-1; i++) {
         for(int j=0;j<counter-1;j++){
@@ -70,5 +95,48 @@
     for (int i=0; i<counter; i++) {
         NSLog(@"%@",[tmp objectAtIndex:i]);
     }
+}
+-(void)sleepsort:(NSMutableArray*)array{
+    NSMutableArray *tmp=[self cpyarray:array from:0 to:[array count]-1];
+    int counter=[tmp count];
+    NSMutableArray *empty=[[NSMutableArray alloc]init];
+    for(int i=0;i<counter;i++){
+        NSNumber *a=[tmp objectAtIndex:i];
+        NSThread *nstd=[[NSThread alloc]initWithTarget:self selector:@selector(slpsub: obj:) object:(array,a)];
+        [nstd start];
+    }
+}
+-(void)slpsub:(NSMutableArray *)array obj:(NSNumber*)time{
+    sleep([time integerValue]);
+    [array addObject:[NSNumber numberWithInt:time]];
+    NSLog(@"%@",time);
+}
+-(void)BucketSort:(NSMutableArray *)array{
+}
+-(void)RadixSprt:(NSMutableArray *)array{//基数排序
+}
+-(void)PigeonholeSort:(NSMutableArray *)array{//鸽巢排序
+}
+-(void)MergeSort:(NSMutableArray *)array{//归并排序
+}
+-(void)SelectSort:(NSMutableArray *)array{//选择排序
+}
+-(void)CocktailSort:(NSMutableArray *)array{//鸡尾酒排序
+}
+-(void)ShellSort:(NSMutableArray *)array{//希尔排序
+}
+-(void)HeapSort:(NSMutableArray *)array{//堆排序
+}
+-(void)GnomeSort:(NSMutableArray *)array{//地精排序
+}
+-(void)OddevenSort:(NSMutableArray *)array{//奇偶排序
+}
+-(void)CombSort:(NSMutableArray *)array{//梳排序
+}
+-(void)PatientSort:(NSMutableArray *)array{//耐心排序
+}
+-(void)BeadSort:(NSMutableArray *)array{//珠排序
+}
+-(void)CountSort:(NSMutableArray *)array{//计数排序
 }
 @end
